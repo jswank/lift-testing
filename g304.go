de97d781dce8c0a74845d10d372c59b844a44490
Jason Swank <jswank@sonatype.com>
2021-06-17 09:23:14 -0400

    add fail function

--- g304.go
+++ g304.go
@@ -21,6 +21,15 @@ func fail_G304() {
 	fmt.Printf("%s", string(byContext))
 }
 
+func fail_G304p2() {
+	repoFile := "/safe/path/../../private/path"
+	byContext, err := ioutil.ReadFile(repoFile)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", string(byContext))
+}
+
 func pass_G304() {
 	repoFile := "/safe/path/../../private/path"
 	repoFile = filepath.Clean(repoFile)
